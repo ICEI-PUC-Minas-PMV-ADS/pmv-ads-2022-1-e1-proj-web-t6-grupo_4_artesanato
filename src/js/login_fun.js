cf3e2be24f701ba1eefb2d91749ee91f0788a419
HenriqueAgp <103278618+HenriqueAgp@users.noreply.github.com>
2022-06-17 15:15:49 -0300

    alteração

--- src/js/login_fun.js
+++ src/js/login_fun.js
@@ -1,13 +1,30 @@
-import {getUser} from "../js/user.js"
 import {UserARepository} from './repositories/UserArepository.js';
+import { getUser, isUser } from './user.js';
+
 const userA_rep = new UserARepository();
 
-function logar () {
-    let login = document.getElementById("nome").value
-    let senha = document.getElementById('senha').value
-    let usuario = userA_rep.getAll().find()
-    
-    alert(login)
-    alert (usuario)
+document.addEventListener ('DOMContentLoaded', () =>{
+    let btn = document.getElementById('btns')
+    btn.addEventListener('click',function () {
+        let login = document.getElementById("nome").value
+        let senha = document.getElementById('senha').value
+        let usuario = userA_rep.getAll().find((u) => u.nome === login && u.senha === senha);
+        
+        if (usuario) {
+           
+            login(usuario);
+            debugger;
+            window.location.href = "usuarioArtesão.html";
+        } else {
+            alert ('Usuário ou senha invalido!');
+            
+        }
+     } );
 
-}
+     if (isUser()) {
+        const user = getUser();
+     }
+})
+
+
+
