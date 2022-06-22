7074e0b77c98414188c993efadfc75d1be024cfc
HenriqueAgp <103278618+HenriqueAgp@users.noreply.github.com>
2022-06-22 17:34:44 -0300

    atualizaçao

--- src/js/login_fun.js
+++ src/js/login_fun.js
@@ -5,17 +5,22 @@ const userA_rep = new UserARepository();
 
 document.addEventListener ('DOMContentLoaded', () =>{
     let btn = document.getElementById('btns')
-    btn.addEventListener('click',function () {
+    btn.addEventListener('click',function (evt) {
+        evt.preventDefault()
         let login = document.getElementById("nome").value
         let senha = document.getElementById('senha').value
         let usuario = userA_rep.getAll().find((u) => u.nome === login && u.senha === senha);
         
         if (usuario) {
-           
-            login(usuario);
-            window.location.href = "http://127.0.0.1:5500/usuarioArtesao.html";
+            
+        
+            window.location.assign("usuarioArtesao.html");
+       
         } else {
+
+
             alert ('Usuário ou senha invalido!');
+            
             
         }
      } );
